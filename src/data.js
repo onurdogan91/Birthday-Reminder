6d687dc8c9bbe5d595a7659c95a9ef1df47abe32
Onur Doğan <106497622+onurdogan91@users.noreply.github.com>
2022-12-19 16:44:43 +0300

    Birthday Reminder

---
+++ src/data.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 1,
+    name: 'Müslüm Gürses',
+    year: 1953,
+    image:
+      'https://upload.wikimedia.org/wikipedia/tr/3/3d/M%C3%BCsl%C3%BCm_G%C3%BCrses_kartpostal.jpg',
+  },
+  {
+    id: 2,
+    name: 'Ferdi Tayfur',
+    year: 1945,
+    image:
+      'https://www.themoviedb.org/t/p/w235_and_h235_face/jipR2FqTwKqRbbt32jLMpcvt1qF.jpg',
+  },
+  {
+    id: 3,
+    name: 'Orhan Gencebay',
+    year: 1944,
+    image: 'https://i.ytimg.com/vi/uiaaHyDj1oc/hqdefault.jpg',
+  },
+  {
+    id: 4,
+    name: 'İbrahim Tatlıses',
+    year: 1952,
+    image:
+      'https://i.discogs.com/kin6j1dXe54NKrDnw1O7sSuRIcvo0MzSgwB3DG-ekvQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTc4MDA4/NjgtMTYxMjA4Mzky/Ny04NjY3LmpwZWc.jpeg',
+  },
+  {
+    id: 5,
+    name: 'Mahmut Tuncer',
+    year: 1961,
+    image: 'https://i.scdn.co/image/ab67616d0000b27345bbb9f26b0976deb5e1cd50',
+  },
+]
